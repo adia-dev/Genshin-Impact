f072ed80e6e2e5c3a7d69cbab65e1b94bd360cfb
pascal <paschy2002@gmail.com>
2022-04-21 01:08:07 +0200

    save changes

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -5,19 +5,34 @@ using UnityEngine;
 public class animationStateController : MonoBehaviour
 {
     Animator animator;
-
+    int isWalkingHash;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+    isWalkingHash = Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("z"))
-        {
-           animator.SetBool("isWalking", true); 
+        bool isRunning = animator.GetBool("isRunning");
+     bool isWalking = animator.GetBool(isWalkingHash);
+    bool forwardPressed = Input.GetKey("z");
+    bool runPressed = Input.GetKey("left shift")
+
+     if (!isWalking && forwardPressed){
+            animator.SetBool("isWalking", true);
+        }
+        if (isWalking && !forwardPressed){
+            animator.SetBool("isWalking", false);
+        }
+
+        if (isRunning && runPressed){
+            animator.SetBool("isRunning", true);
+        }
+        if (!isRunning && !runPressed){
+            animator.SetBool("isRunning", false);
         }
     }
 }

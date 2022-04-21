3c2e00676e72d87e688092e6584516caf28b5a60
pascal <paschy2002@gmail.com>
2022-04-21 02:46:59 +0200

    save

--- Assets/animationStateController.cs
+++ Assets/animationStateController.cs
@@ -6,33 +6,35 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
+    int isRunningHash;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     isWalkingHash = Animator.StringToHash("isWalking");
+    isRunningHash = Animator.StringToHash("isRunning");
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isRunning = animator.GetBool("isRunning");
-     bool isWalking = animator.GetBool(isWalkingHash);
+    bool isRunning = animator.GetBool(isRunningHash);
+    bool isWalking = animator.GetBool(isWalkingHash);
     bool forwardPressed = Input.GetKey("z");
-    bool runPressed = Input.GetKey("left shift")
+    bool runPressed = Input.GetKey("left shift");
 
      if (!isWalking && forwardPressed){
-            animator.SetBool("isWalking", true);
+            animator.SetBool(isWalkingHash, true);
         }
         if (isWalking && !forwardPressed){
-            animator.SetBool("isWalking", false);
+            animator.SetBool(isWalkingHash, false);
         }
 
-        if (isRunning && runPressed){
-            animator.SetBool("isRunning", true);
+        if (!isRunning && (forwardPressed && runPressed)){
+            animator.SetBool(isRunningHash, true);
         }
-        if (!isRunning && !runPressed){
-            animator.SetBool("isRunning", false);
+        if (isRunning && (!forwardPressed || !runPressed)){
+            animator.SetBool(isRunningHash, false);
         }
     }
 }
